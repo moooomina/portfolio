1e09e82ceb960944f9d3b58a7f67009a1c327293
mina <viva3927@google.com>
2024-01-24 18:09:29 +0900

    index

---
+++ script/script.js
@@ -0,0 +1,5 @@
+const container = document.querySelectorAll('.container')
+const btn = document.querySelector('#btn')
+btn.addEventListener('click',function(){
+	window.scrollBy({left:0, top:900})
+})
