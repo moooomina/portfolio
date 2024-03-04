aa6d48873698a253c327e018ab4c9254bfe16da0
mina <viva3927@google.com>
2024-03-04 15:44:20 +0900

    스크롤 수정 필요

--- script/script.js
+++ script/script.js
@@ -1,5 +1,10 @@
-const container = document.querySelectorAll('.container')
+const bg = document.querySelectorAll('.bg')
 const btn = document.querySelector('#btn')
 btn.addEventListener('click',function(){
 	window.scrollBy({left:0, top:800})
 })
+// for(let i=0; i<bg.length; i++){
+//     btn[i].addEventListener('click',()=>{
+//     window.scrollTo({left:0,top:bg[i].offsetTop})
+//     })
+// }
